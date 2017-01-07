fe8662c000a72cb9b960469c17f55db4e64e3156
shaojung <shaojung@gmail.com>
2017-01-07 11:55:40 +0800

    加入 onClickListener

--- app/src/main/java/com/example/student/db010701/MainActivity.java
+++ app/src/main/java/com/example/student/db010701/MainActivity.java
@@ -3,19 +3,33 @@ package com.example.student.db010701;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textView2);
+        btn2 = (Button) findViewById(R.id.button2);
+        myclass c = new myclass();
+        btn2.setOnClickListener(c);
     }
 
     public void click1(View v)
     {
         tv.setText("Hello World");
     }
+    class myclass implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            tv.setText("TT22");
+        }
+    }
 }
+
+
