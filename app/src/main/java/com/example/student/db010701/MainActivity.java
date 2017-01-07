a4809b6ddd4a9b6c755054fc5ae8a5017286ad1a
shaojung <shaojung@gmail.com>
2017-01-07 13:23:44 +0800

    把 Activity 實作 OnClickListener

--- app/src/main/java/com/example/student/db010701/MainActivity.java
+++ app/src/main/java/com/example/student/db010701/MainActivity.java
@@ -6,21 +6,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener{
     TextView tv;
-    Button btn2;
+    Button btn2, btn5, btn6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textView2);
         btn2 = (Button) findViewById(R.id.button2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText("TT22");
-            }
-        });
+        btn2.setOnClickListener(this);
+        btn5 = (Button) findViewById(R.id.button5);
+        btn5.setOnClickListener(this);
+        btn6 = (Button) findViewById(R.id.button6);
+        btn6.setOnClickListener(this);
+
     }
 
     public void click1(View v)
@@ -40,6 +41,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.button2:
+                tv.setText("Hello World2");
+                break;
+            case R.id.button5:
+                tv.setText("Hello World5");
+                break;
+            case R.id.button6:
+                tv.setText("Hello World6");
+                break;
+        }
+
+    }
 }
 
 
